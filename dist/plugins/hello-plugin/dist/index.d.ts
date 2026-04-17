b2a4e117fa33eab027cce9c7e7d791ded413e27f
clawd-a01 <clawd.a01@gmail.com>
2026-04-17 22:50:21 +0000

    chore: stop tracking Nx state and dist artifacts

--- dist/plugins/hello-plugin/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function register(): {
-    name: string;
-    ok: boolean;
-};
--- dist/plugins/hello-plugin/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = register;
-// Minimal placeholder to validate build+deploy plumbing.
-function register() {
-    return { name: 'hello-plugin', ok: true };
-}
-//# sourceMappingURL=index.js.map
